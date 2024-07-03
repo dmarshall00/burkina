123f9e95a03233bb4b79a73c52827dff8f28d9ba
dmarshall00 <mdawn652@gmail.com>
2024-07-03 14:28:06 -0400

    Make list

---
+++ src/scripts/languages.js
@@ -0,0 +1,10 @@
+const languages = [
+    { name: "Bambara", native: "ߓߡߊߣߊ߲ߞߊ߲", code: "bm" },
+    { name: "English", native: "English", code: "en" },
+    { name: "Fulfulde - West Niger", native: "غٛرْغَلْ نِيسٜرْ فُلْفُلْدٜ", code: "fuh" },
+    { name: "Grebo", native: "Kréébo", code: "grb" },
+    { name: "Sudanese Arabic", native: "لهجة سودانية", code: "apd" },
+    { name: "Tawellemmet", native: "ⵜⵓⵍⵎⵓⵜ", code: "ttq" },
+    { name: "Wolof", native: "Wolof làkk", code: "wo" },
+    { name: "Yoruba", native: "Yorùbá", code: "yo" }
+];
